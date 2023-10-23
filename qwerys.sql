52f40926ec2518afa3401c4d020513aff97c2aa6
Carlos Adiel Reyes Soto <carlosreyes1803@gmail.com>
2023-10-23 10:40:11 -0600

    Se le hicieron cambio a la base de datos y se subio a Informix

--- qwerys.sql
+++ qwerys.sql
@@ -71,37 +71,13 @@ where per_catalogo =--catalogo
 
 
 --//!BASE DE DATOS
-
-
--- Tabla Contingente
-CREATE TABLE contingentes (
-    cont_id SERIAL PRIMARY KEY,
-    cont_nombre CHAR(150),
-    cont_destino INT NOT NULL,
-    cont_fecha_pre DATETIME YEAR TO MINUTE NOT NULL,
-    cont_fecha_inicio DATETIME YEAR TO MINUTE NOT NULL,
-    cont_fecha_final DATETIME YEAR TO MINUTE NOT NULL,
-    cont_fecha_post DATETIME YEAR TO MINUTE NOT NULL,
-    cont_situacion SMALLINT,
-     FOREIGN KEY (cont_destino) REFERENCES cont_destinos(dest_id)
-);
-
 -- Tabla Cont_Destino
 CREATE TABLE cont_destinos (
     dest_id SERIAL PRIMARY KEY,
     dest_nombre VARCHAR(200)NOT NULL,
     dest_latitud DECIMAL(10, 6) NOT NULL,
     dest_longitud DECIMAL(10, 6) NOT NULL,
-    dest_situacion SMALLINT,
-)
-
-
--- Tabla Cont_Aprovados
-CREATE TABLE cont_aprovados (
-    apro_id SERIAL PRIMARY KEY,
-    apro_asp INT,
-    apro_situacion SMALLINT,
-    FOREIGN KEY (apro_asp) REFERENCES Cont_Ingresos(ing_asp)
+    dest_situacion SMALLINT
 );
 
 -- Tabla Cont_Evaluaciones
@@ -128,29 +104,67 @@ CREATE TABLE cont_aspirantes (
     asp_nom2 CHAR(50),
     asp_ape1 CHAR(50),
     asp_ape2 CHAR(50),
-    asp_genero CHAR(50),
-    asp_situacion SMALLINT,
+    asp_genero CHAR(1),
+    asp_situacion SMALLINT
 );
 
-
--- Tabla Cont_Plazas
-CREATE TABLE cont_plazas();
-
-
+-- Tabla Contingente
+CREATE TABLE contingentes (
+    cont_id SERIAL PRIMARY KEY,
+    cont_nombre CHAR(150),
+    cont_destino INT NOT NULL,
+    cont_fecha_pre DATETIME YEAR TO MINUTE NOT NULL,
+    cont_fecha_inicio DATETIME YEAR TO MINUTE NOT NULL,
+    cont_fecha_final DATETIME YEAR TO MINUTE NOT NULL,
+    cont_fecha_post DATETIME YEAR TO MINUTE NOT NULL,
+    cont_situacion SMALLINT,
+    FOREIGN KEY (cont_destino) REFERENCES cont_destinos(dest_id)
+);
 
 -- Tabla Cont_Ingresos
 CREATE TABLE cont_ingresos (
     ing_id SERIAL PRIMARY KEY,
     ing_asp INT,
     ing_contingente INT,
-    ing_plaza INT,
     ing_fecha_cont DATETIME YEAR TO MINUTE NOT NULL,
     ing_puesto INT,
     ing_situacion SMALLINT,
-    FOREIGN KEY (ing_puesto) REFERENCES cont_puestos(pue_id)
+    FOREIGN KEY (ing_puesto) REFERENCES cont_puestos(pue_id),
     FOREIGN KEY (ing_asp) REFERENCES cont_aspirantes(asp_id),
-    FOREIGN KEY (ing_contingente) REFERENCES contingente(cont_id)
+    FOREIGN KEY (ing_contingente) REFERENCES contingentes(cont_id)
 );
+
+
+-- Tabla Cont_Aprovados
+CREATE TABLE cont_aprovados (
+    apro_id SERIAL PRIMARY KEY,
+    apro_asp INT,
+    apro_situacion SMALLINT,
+    FOREIGN KEY (apro_asp) REFERENCES cont_ingresos(ing_id)
+);
+
+
+CREATE TABLE cont_plazas (
+    plaz_id SERIAL PRIMARY KEY,
+    plaz_codigo CHAR(30) UNIQUE,
+    plaz_nombre CHAR(100),
+    plaz_grado SMALLINT,
+    plaz_situacion SMALLINT,
+    FOREIGN KEY (plaz_grado) REFERENCES grados(gra_codigo)
+);
+
+-- Tabla cont_asig_plazas
+CREATE TABLE cont_asig_plazas(
+    asig_id SERIAL PRIMARY KEY,
+    asig_situacion SMALLINT,
+    asig_puesto INT,
+    asig_plaza INT, 
+    asig_contingente INT,
+    FOREIGN KEY (asig_puesto) REFERENCES cont_puestos (pue_id),
+    FOREIGN KEY (asig_plaza) REFERENCES cont_plazas (plaz_id),
+    FOREIGN KEY (asig_contingente) REFERENCES contingentes (cont_id)
+);
+
 
 -- Tabla Cont_Resultado
 CREATE TABLE cont_resultados (
@@ -164,71 +178,19 @@ CREATE TABLE cont_resultados (
 );
 
 
+DROP TABLE contingentes
+DROP TABLE cont_aprovados
+DROP TABLE cont_evaluaciones
+DROP TABLE cont_puestos
+DROP TABLE cont_aspirantes
+DROP TABLE cont_ingresos
+DROP TABLE cont_resultados
+DROP TABLE cont_destinos
+DROP TABLE cont_plazas
+DROP TABLE cont_asig_plazas
+
+
 --!Datos
-
--- Ingresar datos en la tabla Contingente
-INSERT INTO contingente (cont_id, cont_destino, cont_fecha_pre, cont_fecha_inicio, cont_fecha_final, cont_fecha_post, cont_situacion)
-VALUES
-    (1, 'Congo 2020', '2020-10-17 08:00', '2020-10-18 08:00', '2020-10-25 08:00', '2020-10-30 08:00', 1),
-    (2, 'Congo 2021', '2021-10-20 08:00', '2021-10-21 08:00', '2021-10-28 08:00', '2021-11-02 08:00', 1),
-    (3, 'Congo 2022', '2022-10-25 08:00', '2022-10-26 08:00', '2022-11-02 08:00', '2022-11-07 08:00', 1),
-    (4, 'Congo 2023', '2023-10-30 08:00', '2023-10-31 08:00', '2023-11-07 08:00', '2023-11-12 08:00', 1),
-    (5, 'Congo 2024', '2024-11-05 08:00', '2024-11-06 08:00', '2024-11-13 08:00', '2024-11-18 08:00', 1);
-
--- Ingresar datos en la tabla Cont_Aprovados
-INSERT INTO cont_aprovados (apro_id, apro_asp, apro_situacion)
-VALUES
-    (1, 1, 1),
-    (2, 2, 1),
-    (3, 3, 1),
-    (4, 4, 1),
-    (5, 5, 1);
-
--- Ingresar datos en la tabla Cont_Evaluaciones
-INSERT INTO cont_evaluaciones (eva_id, eva_nombre, eva_situacion)
-VALUES
-    (1, 'Evaluación 1', 1),
-    (2, 'Evaluación 2', 1),
-    (3, 'Evaluación 3', 1),
-    (4, 'Evaluación 4', 1),
-    (5, 'Evaluación 5', 1);
-
--- Ingresar datos en la tabla Cont_Puestos
-INSERT INTO cont_puestos (pue_id, pue_nombre, pue_grado, pue_situacion)
-VALUES
-    (1, 'Puesto 1', 10, 1),
-    (2, 'Puesto 2', 12, 1),
-    (3, 'Puesto 3', 9, 1),
-    (4, 'Puesto 4', 11, 1),
-    (5, 'Puesto 5', 8, 1);
-
--- Ingresar datos en la tabla Cont_Aspirantes
-INSERT INTO cont_aspirantes (asp_id, asp_catalogo, asp_nom1, asp_nom2, asp_ape1, asp_ape2, asp_genero, asp_situacion, asp_puesto)
-VALUES
-    (1, 1001, 'Carlos', 'Adiel', 'Reyes', 'Soto', 'Masculino', 1, 1),
-    (2, 1002, 'Kenser', 'Omar', 'Caal', 'Juc', 'Masculino', 1, 2),
-    (3, 1003, 'Livni', 'Nohemi', 'Martinez', 'Canahui', 'Femenino', 1, 3),
-    (4, 1004, 'Lester', 'Maudiel', 'Franco', 'Lopez', 'Masculino', 1, 4),
-    (5, 1005, 'Sergio', 'Danilo', 'Bolvito', 'Rodriguez', 'Masculino', 1, 5);
-
--- Ingresar datos en la tabla Cont_Ingresos
-INSERT INTO cont_ingresos (ing_id, ing_asp, ing_contingente, ing_plaza, ing_fecha_cont, ing_situacion)
-VALUES
-    (1, 1, 1, 101, '2023-10-19 08:00', 1),
-    (2, 2, 2, 102, '2023-10-22 08:00', 1),
-    (3, 3, 3, 103, '2023-10-27 08:00', 1),
-    (4, 4, 4, 104, '2023-11-01 08:00', 1),
-    (5, 5, 5, 105, '2023-11-06 08:00', 1);
-
--- Ingresar datos en la tabla Cont_Resultado
-INSERT INTO cont_resultado (res_id, res_asp, res_contingente, res_nota, res_evaluacion, res_situacion)
-VALUES
-    (1, 1, 1, 85.50, 1, 1),
-    (2, 2, 2, 78.25, 2, 1),
-    (3, 3, 3, 92.75, 3, 1),
-    (4, 4, 4, 70.00, 4, 1),
-    (5, 5, 5, 89.20, 5, 1);
-Asegúrate de ajustar los valores de los datos según tus necesidades reales. Ten en cuenta que las fechas y horas deben seguir el formato 'YYYY-MM-DD HH:MM' para DATETIME YEAR TO MINUTE.
 
 
 
